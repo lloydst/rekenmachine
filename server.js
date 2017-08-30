5416a0b69975fa6dbb49e9057d54af4c6cf58dcc
lloydst <lloyd.stumpel@gmail.com>
2017-08-30 15:54:22 +0200

    added readme

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ var express = require('express');
 
 // starting 3 seperate servers 
 var html = express();
-var jquerry = express();
+var jquery = express();
 var angularjs = express();
 
 // serving folders using express
 html.use(express.static('htmlcalc'));
-jquerry.use(express.static('jquerrycalc'));
+jquery.use(express.static('jquerycalc'));
 angularjs.use(express.static('angularjscalc'));
 
 // telling the servers what port to use
 html.listen(3000);
-jquerry.listen(3001);
+jquery.listen(3001);
 angularjs.listen(3002);
 
 console.log('the calculators can be found at:')
 console.log('html: localhost:3000')
-console.log('jquerry: localhost:3001')
+console.log('jquery: localhost:3001')
 console.log('angularjs: localhost:3002')
