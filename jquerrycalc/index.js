1d99ff78b69f21bf2c3b8cb6cacdc3b2e60b8144
lloydst <lloyd.stumpel@gmail.com>
2017-08-30 12:40:27 +0200

    jquerry

--- jquerrycalc/index.js
+++ jquerrycalc/index.js
@@ -1,7 +1,55 @@
-//#target is a id .target is a class
-
+// start this script once html is loaded
 $(document).ready(function(){
-    $('.result').html(0);   //$('p').html("replaces text within the first tag it finds");
-
+    // some vars
+    var result = 0;
+    var storage = 0
+    var operation = null;
+    var currentEntry= '0';
+    updateResult(result);
     
-});
+    // gets the value of the pressed button and store's it in storage
+    $('input:button').click(function(){
+        var buttonPressed = $(this).val()
+        console.log(buttonPressed)
+        //dealing with C and equal key + bussiness logic
+        if (buttonPressed === 'C'){
+            result = 0
+            currentEntry = '0'
+        } else if(buttonPressed === '=') {
+            currentEntry = operate(prevEntry, currentEntry, operation);
+            operation = null;
+        }  else if (isNumber(buttonPressed)) {
+            if (currentEntry === '0') currentEntry = buttonPressed;
+            else currentEntry = currentEntry + buttonPressed;
+        } else if (isOperator(buttonPressed)) {
+            prevEntry = parseFloat(currentEntry);
+            operation = buttonPressed;
+            currentEntry = ' ';
+        }
+        updateResult(currentEntry);
+    })
+
+})
+// functions
+updateResult = function(displayValue) {
+  var displayValue = displayValue.toString();
+  $('.result').html(displayValue.substring(0, 10));
+};
+
+isNumber = function(value) {
+  return !isNaN(value);
+}
+
+isOperator = function(value) {
+  return value === '/' || value === '*' || value === '+' || value === '-';
+};
+
+operate = function(a, b, operation) {
+  a = parseFloat(a);
+  b = parseFloat(b);
+  console.log(a, b, operation);
+  if (operation === '+') return a + b;
+  if (operation === '-') return a - b;
+  if (operation === '*') return a * b;
+  if (operation === '/') return a / b;
+}
